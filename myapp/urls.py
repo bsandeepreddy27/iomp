290f4d04ae0e0ef65f0eb5e911d30a722e6a4275
bsandeepreddy27 <bsandeepreddy27@gmail.com>
2024-06-05 16:45:54 +0530

    2

--- myapp/urls.py
+++ myapp/urls.py
@@ -7,13 +7,11 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('cases/', views.case_list, name='case_list'),
     path('cases/add/', views.add_case, name='add_case'),
-
     path('home', views.home_view, name='home_view'),
-  
     path('login/', views.login_view, name='login'),
     path('signup/', views.signup_view, name='signup'),
     path('cases/<int:case_id>/edit/', views.edit_case, name='edit_case'),
+    path('change-password/', views.change_password, name='change_password'),
+    path('logout/', views.custom_logout, name='logout'),
 ]
-
-
 
